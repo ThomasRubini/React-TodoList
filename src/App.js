b340a7f9da9b56a7c0b6d43e3939ec48ff34b42e
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2023-03-13 10:24:32 +0100

    add Task and TaskModel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
 
+function ensureVar(arg, type){
+  if(arg===null||arg===undefined){
+    throw new Error("argument is not defined");
+  }
+  if (type){
+    if(typeof(arg) !== type){
+      throw new Error("argument type is invalid");
+    }
+  }
+  return arg;
+}
+
+class TaskModel{
+  constructor(done, desc){
+    this.done = ensureVar(done, 'boolean');
+    this.desc = ensureVar(desc, 'string');
+  }
+}
+
+class Task extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      done: ensureVar(this.props.model.done, 'boolean'),
+      desc: ensureVar(this.props.model.desc, 'string')
+    };
+  }
+
+  onTaskDescEdit(e) {
+    this.setState({desc: e.target.value});
+  }
+
+  render() {
+    return (
+      <section key={this.key}>
+        <input type="checkbox" defaultChecked={this.state.done}></input>
+        <input type="text" value={this.state.desc} onChange={(e) => this.onTaskDescEdit(e)}></input>
+      </section>
+    )
+  }
+}
 function App() {
+  let taskModels = [
+    new TaskModel(true, "Some random task"),
+    new TaskModel(false, "Finishing this app")
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section id="tasks">
+        { taskModels.map((task, i) => <Task key={i} model={task}></Task>)}
+        <section id="add">
+          <button>Add new task</button>
+        </section>
+      </section>
     </div>
   );
 }
